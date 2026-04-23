1cb9ae08d6706b370f3f458caff2fc8c7423a851
ylabate <12196@holbertonstudents.com>
2026-04-23 17:01:24 +0200

    Docs(NoSQL): Add docstrings to update_topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
+"""Module that updates topics for a document in a MongoDB collection"""
+
+
 def update_topics(mongo_collection, name, topics):
+    """Updates the topics of a document by name
+
+    Args:
+        mongo_collection: The pymongo collection object
+        name: The name of the document to update
+        topics: A list of topics to set for the document
+
+    Returns:
+        None
+    """
     mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
