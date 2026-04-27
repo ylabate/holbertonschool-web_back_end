4f1f2cd63cad7a31593b4e1be0112a971d06bb32
ylabate <12196@holbertonstudents.com>
2026-04-27 16:43:08 +0200

    fix(loop): Iterate array values directly

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const res = [];
-  for (const value of array.entries()) {
+  for (const value of array) {
     res.push(appendString + value);
   }
 
