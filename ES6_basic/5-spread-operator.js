0d8583d4fe859d279a2592b6bb08e3030666c0c8
ylabate <12196@holbertonstudents.com>
2026-04-27 15:53:20 +0200

    Add concatArrays using spread operator

---
+++ ES6_basic/5-spread-operator.js
@@ -0,0 +1,5 @@
+// export default function concatArrays(array1, array2, string) {}
+
+const concatArrays = (array1, array2, string) => [...array1, ...array2, ...string]
+
+export default concatArrays;
