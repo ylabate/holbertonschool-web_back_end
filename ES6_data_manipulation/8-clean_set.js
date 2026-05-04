c6cc7dbc69db437907a60ffea570a08c02de9c5e
ylabate <12196@holbertonstudents.com>
2026-05-04 15:22:57 +0200

    Feat(cleanSet): add function to clean set strings

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,8 @@
+export default function cleanSet(set, startString) {
+  if (!startString) return ''
+  const res = [];
+  for (const string of set) {
+    if (string.startsWith(startString)) { res.push(string.substring(startString.length)); }
+  }
+  return res.join('-');
+}
