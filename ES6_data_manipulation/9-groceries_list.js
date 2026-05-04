7e6829276fc77a3a165a6e2d4326ec2640b2fced
ylabate <12196@holbertonstudents.com>
2026-05-04 17:10:12 +0200

    Feat(groceries): add groceriesList returning Map

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const result = new Map()
+
+  result.set('Apples', 10);
+  result.set('Tomatoes', 10);
+  result.set('Pasta', 1);
+  result.set('Rice', 1);
+  result.set('Banana', 5);
+
+  return result
+}
