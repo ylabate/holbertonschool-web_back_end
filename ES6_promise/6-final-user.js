27d4e2ca8ba5f8798ab86f49c59bca6839ab7d31
ylabate <12196@holbertonstudents.com>
2026-05-08 10:58:28 +0200

    Fix(promise): Stringify rejection reason

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,6 +5,6 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((results) => results.map((res) => ({
       status: res.status,
-      value: res.status === 'fulfilled' ? res.value : res.reason,
+      value: res.status === 'fulfilled' ? res.value : res.reason.toString(),
     })));
 }
