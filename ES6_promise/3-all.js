f5479795a535d94526d988dbe28dc654441688b1
ylabate <12196@holbertonstudents.com>
2026-05-08 09:44:20 +0200

    feat(promise): Add profile signup handler
    Use Promise.all to upload photo and create user concurrently.
    Log combined results on success and 'Signup system offline' on failure.
    Add 3-main.js to invoke the handler.

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from 'utils.js'
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    })
+}
