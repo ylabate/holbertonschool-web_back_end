78b3614890f59acfe0e6e00f11472824fe905c8f
ylabate <12196@holbertonstudents.com>
2026-05-08 10:56:27 +0200

    Refactor(promises): normalize allSettled output

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,4 +3,8 @@ import uploadPhoto from './5-photo-reject'
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((res) => ({
+      status: res.status,
+      value: res.status === 'fulfilled' ? res.value : res.reason,
+    })));
 }
