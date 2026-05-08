50c5d9c542210154119d13b25b59a76840c8ef8c
ylabate <12196@holbertonstudents.com>
2026-05-08 09:47:22 +0200

    Fix: Use relative import for utils

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from 'utils.js'
+import { uploadPhoto, createUser } from './utils.js'
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
