01805a908b26d2f5004b47b722bb4c5e46b2c00f
ylabate <12196@holbertonstudents.com>
2026-05-12 21:09:44 +0200

    feat: Add async student counter and demo scripts

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf-8')
+    .then((data) => data.trim().split('\n').filter((line) => line.trim().length > 0))
+    .then((lines) => lines.map((line) => line.split(',')).slice(1))
+    .then((csv) => {
+      console.log(`Number of students: ${csv.length}`);
+
+      const fields = {};
+      for (const student of csv) {
+        const fieldName = student[3];
+        if (!fields[fieldName]) {
+          fields[fieldName] = { number: 0, students: [] };
+        }
+        fields[fieldName].number += 1;
+        fields[fieldName].students.push(student[0]);
+      }
+      for (const [field, value] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${value.number}. List: ${value.students.join(', ')}`);
+      }
+    })
+    .catch(() => {
+      throw Error('Cannot load the database');
+    })
+}
+
+module.exports = countStudents;
