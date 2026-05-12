7042d2ce5d85b0115edd55e786a68ab64b920607
ylabate <12196@holbertonstudents.com>
2026-05-12 06:10:05 +0200

    Fix: Exit process when readline closes

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
@@ -12,4 +12,5 @@ rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
 
 rl.on('close', () => {
   console.log('This important software is now closing');
+  process.exit(0);
 });
