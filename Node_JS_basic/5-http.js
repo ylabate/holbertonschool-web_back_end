8c869374f710dc232d9c0f632ff0d7d9d48f26c6
ylabate <12196@holbertonstudents.com>
2026-05-12 23:26:30 +0200

    Refactor: make countStudents return a Promise

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,31 +1,30 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.trim().split('\n').filter((line) => line.length > 0);
-    const csv = lines.map((line) => line.split(',')).slice(1);
+  return fs.readFile(path, 'utf-8')
+    .then((data) => data.trim().split('\n').filter((line) => line.trim().length > 0))
+    .then((lines) => lines.map((line) => line.split(',')).slice(1))
+    .then((csv) => {
+      let res = `Number of students: ${csv.length}`;
 
-    const fields = {};
-    for (const student of csv) {
-      const fieldName = student[3];
-      if (!fields[fieldName]) {
-        fields[fieldName] = { number: 0, students: [] };
+      const fields = {};
+      for (const student of csv) {
+        const fieldName = student[3];
+        if (!fields[fieldName]) {
+          fields[fieldName] = { number: 0, students: [] };
+        }
+        fields[fieldName].number += 1;
+        fields[fieldName].students.push(student[0]);
       }
-      fields[fieldName].number += 1;
-      fields[fieldName].students.push(student[0]);
-    }
-
-    let res = `Number of students: ${csv.length}`;
-
-    for (const [field, value] of Object.entries(fields)) {
-      res += (`\nNumber of students in ${field}: ${value.number}. List: ${value.students.join(', ')}`);
-    }
-    return res;
-  } catch (_) {
-    throw Error('Cannot load the database');
-  }
+      for (const [field, value] of Object.entries(fields)) {
+        res += `\nNumber of students in ${field}: ${value.number}. List: ${value.students.join(', ')}`;
+      }
+      return res;
+    })
+    .catch((err) => {
+      throw Error('Cannot load the database');
+    });
 }
 
 const app = http.createServer((req, res) => {
@@ -34,9 +33,16 @@ const app = http.createServer((req, res) => {
     res.write('Hello Holberton School!');
   } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
-    res.write(countStudents(process.argv[2]));
+    countStudents(process.argv[2])
+      .then((studentList) => {
+        res.write(studentList);
+        res.end();
+      })
+      .catch((err) => {
+        res.write(err.message);
+        res.end();
+      })
   }
-  res.end();
 });
 
 app.listen(1245);
