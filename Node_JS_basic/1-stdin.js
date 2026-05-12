db90b3cba098ce6f0c77d4baeadae38e900efd97
ylabate <12196@holbertonstudents.com>
2026-05-12 06:04:16 +0200

    fix: remove old methode residue

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,11 +7,10 @@ const rl = readline.createInterface({
 
 console.log('Welcome to Holberton School, what is your name?');
 
-const reponse = await rl.question();
-
 rl.question('', (answer) => {
   console.log(`Your name is: ${answer}`);
   rl.close();
 });
+
 
 console.log('This important software is now closing');
