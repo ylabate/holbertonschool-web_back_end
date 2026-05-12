1da4d722aad75c43e651c72a997d3f9f34da2c59
ylabate <12196@holbertonstudents.com>
2026-05-12 06:11:55 +0200

    fix(stdin): Only log closing when stdin is not TTY

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -11,6 +11,8 @@ rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
 });
 
 rl.on('close', () => {
-  console.log('This important software is now closing');
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
   process.exit(0);
 });
