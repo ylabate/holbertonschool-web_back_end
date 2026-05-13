529a7799d5088a8e86dd77f13a6cefe82e07b658
ylabate <12196@holbertonstudents.com>
2026-05-13 02:47:07 +0200

    chore: added missing semicolon

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,14 +1,14 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = 1245
+const app = express();
+const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!')
-})
+  res.send('Hello Holberton School!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
 
 module.exports = app;
